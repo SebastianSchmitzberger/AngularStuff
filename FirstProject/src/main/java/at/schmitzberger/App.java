00aa41e256892122c7becb21cebe5492b49cf67a
SebastianSchmitzberger <Sebastian.Schmitzberger@gmx.at>
2017-01-29 18:00:55 +0100

    finished first tutorial jar, it works

--- FirstProject/src/main/java/at/schmitzberger/App.java
+++ FirstProject/src/main/java/at/schmitzberger/App.java
@@ -2,18 +2,15 @@ package at.schmitzberger;
 
 import java.util.UUID;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App obj = new App();
+        System.out.println("Unique ID : " + obj.generateUniqueKey());
     }
+
     public String generateUniqueKey() {
-        String id = UUID.randomUUID().toString();
-        return id;
+        return UUID.randomUUID().toString();
     }
 }
